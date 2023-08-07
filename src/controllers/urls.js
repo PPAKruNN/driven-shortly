10198cc5f05c3cf9f59e6102db0d0c4d2e508707
Pedro Nascimento <sn.jaypee@gmail.com>
2023-08-07 01:16:06 -0300

    Feat: add GETs: Url by ID, Open URL

---
+++ src/controllers/urls.js
@@ -0,0 +1,70 @@
+import { v4 } from "uuid";
+import { db } from "../database.js";
+import bcrypt from "bcrypt";
+import { nanoid } from "nanoid";
+
+export async function shortenUrl(req, res ) {
+    
+    try {
+        const { url } = req.body;
+        const userId = res.locals.id;
+        const shortenUrl = nanoid();
+
+        const insertShortenUrl = await db.query(`
+        INSERT INTO shortUrls(userid, shorturl, url) 
+        VALUES ($1, $2, $3)
+        RETURNING id; 
+        `, [userId, shortenUrl, url]);
+
+        const responseObject = {
+            "id": insertShortenUrl.rows[0].id,
+            "shortUrl": shortenUrl
+        }
+
+        res.status(201).send(responseObject);
+
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+}
+
+export async function getUrlById(req, res ) {
+    try {
+        const { id } = req.params;
+
+        const urlSearch = await db.query(`
+        SELECT id, shorturl, url
+        FROM shortUrls
+        WHERE id = $1; 
+        `, [ id ])
+        
+        
+        if(urlSearch.rowCount === 0) return res.sendStatus(404);
+
+        res.status(200).send(urlSearch.rows[0]);
+
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+}
+
+
+export async function openUrl(req, res ) {
+    try {
+        const { shortUrl } = req.params;
+
+        const urlInsert = await db.query(`
+            UPDATE shorturls
+            SET visitcount = visitcount + 1
+            WHERE shorturl = $1
+            RETURNING url;
+        `, [shortUrl])
+        
+        if(urlInsert.rowCount === 0) return res.sendStatus(404);
+
+        res.redirect(urlInsert.rows[0].url);
+
+    } catch (error) {
+        return res.sendStatus(500);
+    }
+}
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import validateSchema from "../middleware/validateSchema.js";
 import authenticate from "../middleware/authenticate.js";
+import urlShortenSchema from "../schemas/urlShortenSchema.js";
+import { getUrlById, openUrl, shortenUrl } from "../controllers/urls.js";
 
 const router = Router();
 
-router.post("/urls/shorten", authenticate )
+router.post("/urls/shorten", validateSchema(urlShortenSchema), authenticate, shortenUrl )
+router.get("/urls/:id", getUrlById)
+router.get("/urls/open/:shortUrl", openUrl)
 
 
 export default router;
